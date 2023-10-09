5bd328079925a7a66b90a186bad8910447d1c08d
Selvakumar <vaithiselva123@gmail.com>
2023-10-09 10:56:12 +0530

    responsive commit

---
+++ reponsive.js
@@ -0,0 +1,14 @@
+let ul=document.querySelector('.home');
+let but=document.querySelector('.hum');
+but.addEventListener('click', sel)
+function sel(){
+    ul.classList.toggle('slide')
+}
+
+
+const form=document.querySelector('#name'),
+      email=document.querySelector('#email'),
+      sub=document.querySelector('#subject'),
+      msg=document.querySelector('#message')
+
+   
